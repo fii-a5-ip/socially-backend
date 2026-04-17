e7fa93ca1e9b680201a1146f9c1eb61671d67994
BusmachiuEduard <eduardbusmachiu@gmail.com>
2026-04-17 17:24:56 +0300

    Entity Filter, Group

--- backend/backend/src/main/java/com/soccialy/backend/entity/Filter.java
+++ backend/backend/src/main/java/com/soccialy/backend/entity/Filter.java
@@ -1,4 +1,36 @@
 package com.soccialy.backend.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "filters")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Filter {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, nullable = false, length = 45)
+    private String name;
+
+    @Builder.Default
+    @ManyToMany(mappedBy = "filters")
+    private Set<User> users = new HashSet<>();
+
+    @Builder.Default
+    @ManyToMany(mappedBy = "filters")
+    private Set<Location> locations = new HashSet<>();
+
+    public Filter(String name) {
+        this.name = name;
+    }
 }
--- backend/backend/src/main/java/com/soccialy/backend/entity/Group.java
+++ backend/backend/src/main/java/com/soccialy/backend/entity/Group.java
@@ -1,4 +1,37 @@
 package com.soccialy.backend.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "groups") // Notă: 'groups' este cuvânt rezervat în MySQL, dar JPA se descurcă dacă e setat corect.
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Group {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 45)
+    private String name;
+
+    @Builder.Default
+    @ManyToMany
+    @JoinTable(
+            name = "group_users",
+            joinColumns = @JoinColumn(name = "group_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private Set<User> users = new HashSet<>();
+
+    public Group(String name) {
+        this.name = name;
+    }
 }
