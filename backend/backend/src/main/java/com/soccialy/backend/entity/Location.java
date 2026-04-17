d3ea8fa38f9f480d95268a7b8aed21e40abf2411
Mironescu Ioana <ioanabianca2017@gmail.com>
2026-04-17 17:04:05 +0300

    Entity Location, Outgoing

--- backend/backend/src/main/java/com/soccialy/backend/entity/Location.java
+++ backend/backend/src/main/java/com/soccialy/backend/entity/Location.java
@@ -1,4 +1,41 @@
 package com.soccialy.backend.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "locations")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Location {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "location", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Outgoing> outgoings = new HashSet<>();
+
+    @Builder.Default
+    @ManyToMany
+    @JoinTable(
+            name = "location_filters",
+            joinColumns = @JoinColumn(name = "location_id"),
+            inverseJoinColumns = @JoinColumn(name = "filter_id")
+    )
+    private Set<Filter> filters = new HashSet<>();
+
+    public Location(String name) {
+        this.name = name;
+    }
 }
