4002d632690b36937e4dbdfaff787e04befe0a1c
DN00Andrei <162124186+DN00Andrei@users.noreply.github.com>
2026-05-01 19:09:14 +0300

    Remove unused TestPropertySource annotation

--- backend/backend/src/test/java/com/soccialy/backend/BackendApplicationTests.java
+++ backend/backend/src/test/java/com/soccialy/backend/BackendApplicationTests.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@TestPropertySource("classpath:application.properties") // Nu gaseste Spring calea catrea test/resources/application.properties. Încer asta.
 class BackendApplicationTests {
 
 	@Test
