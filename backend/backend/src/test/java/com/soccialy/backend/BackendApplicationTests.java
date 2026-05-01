38c9ec85e50fb6b0ac51759dd1493dc8441147c8
DN00Andrei <162124186+DN00Andrei@users.noreply.github.com>
2026-05-01 19:06:01 +0300

    Specify properties file for BackendApplicationTests
    Add TestPropertySource annotation to specify properties file location for tests.

--- backend/backend/src/test/java/com/soccialy/backend/BackendApplicationTests.java
+++ backend/backend/src/test/java/com/soccialy/backend/BackendApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@TestPropertySource("classpath:application.properties") // Nu gaseste Spring calea catrea test/resources/application.properties. Încer asta.
 class BackendApplicationTests {
 
 	@Test
