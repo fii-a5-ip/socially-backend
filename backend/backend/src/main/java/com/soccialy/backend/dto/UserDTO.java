21a839056e023e513b365a089d7d4b97ce3cdb88
cris01Andrei <162047378+cris01Andrei@users.noreply.github.com>
2026-05-07 18:39:33 +0300

    Add fullname field and reorganize UserDTO

--- backend/backend/src/main/java/com/soccialy/backend/dto/UserDTO.java
+++ backend/backend/src/main/java/com/soccialy/backend/dto/UserDTO.java
@@ -1,19 +1,16 @@
 package com.soccialy.backend.dto;
 
 import lombok.Data;
-
 import java.util.List;
 
 @Data
 public class UserDTO {
     private Integer id;
     private String username;
+    private String fullname;
     private String email;
     private String bio;
     private String profilePictureUrl;
-    private List<FilterDTO> filters;
-}
-    private String fullname;
-    private String email;
     private String profileImgUrl;
+    private List<FilterDTO> filters;
 }
