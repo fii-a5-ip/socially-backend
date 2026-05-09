c294da132aa17338bd0a5974613aad998b16dac1
BusmachiuEduard <eduardbusmachiu@gmail.com>
2026-05-09 16:40:23 +0300

    Repository

--- backend/backend/src/main/java/com/soccialy/backend/repository/UserRepository.java
+++ backend/backend/src/main/java/com/soccialy/backend/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.soccialy.backend.repository;
 import com.soccialy.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -10,6 +12,13 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByUsername(String username);
     
     boolean existsByUsername(String username); //Am adăugat această metodiă fiindcaă SonarQube o caută atunci când face teste. Metoda ar trebui să fie implementată de Spring
+
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+    Optional<User> findByGoogleId(String googleId);
+    List<User> findByUsernameStartingWithIgnoreCase(String prefix);
+    List<User> findByGroupsId(Integer groupId);
+    List<User> findByFiltersId(Integer filterId);
 
     @org.springframework.data.jpa.repository.Query("SELECT u FROM User u WHERE " +
            "LOWER(u.username) LIKE LOWER(CONCAT('%', :query, '%')) OR " +
