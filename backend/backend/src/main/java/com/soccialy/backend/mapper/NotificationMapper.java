f50067bec8ae2c883991f071ce6ee165ce707479
DN00Andrei <162124186+DN00Andrei@users.noreply.github.com>
2026-05-19 08:56:36 +0300

    Update NotificationMapper.java

--- backend/backend/src/main/java/com/soccialy/backend/mapper/NotificationMapper.java
+++ backend/backend/src/main/java/com/soccialy/backend/mapper/NotificationMapper.java
@@ -3,13 +3,23 @@ package com.soccialy.backend.mapper;
 import com.soccialy.backend.dto.NotificationDTO;
 import com.soccialy.backend.entity.Notification;
 import org.springframework.stereotype.Component;
+import com.soccialy.backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 
+
+@RequiredArgsConstructor
 @Component
 public class NotificationMapper {
 
-    public NotificationDTO toDTO(
-            Notification notification
-    ){
+    private final UserRepository userRepository;
+
+    public NotificationDTO toDTO( Notification notification )
+        {
+                if (notification == null) { return null; }
+
+                // prevenire crash în caz că ”nu există” users
+            var user = (notification.getActorUserId() != null)
+              ? userRepository.findById(notification.getActorUserId()).orElse(null) : null;
 
         return NotificationDTO.builder()
                 .id(notification.getId())
@@ -17,7 +27,7 @@ public class NotificationMapper {
                         notification.getActorUserId()
                 )
                 .type(
-                        notification.getType().name()
+                        notification.getType() != null ? notification.getType().name() : null
                 )
                 .message(
                         notification.getMessage()
@@ -34,6 +44,10 @@ public class NotificationMapper {
                 .createdAt(
                         notification.getCreatedAt()
                 )
+
+                .username(user != null ? user.getUsername() : null)
+                .profileImageUrl(user != null ? user.getProfileImgUrl() : null)
+
                 .build();
 
     }
